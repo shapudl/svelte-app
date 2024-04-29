c17f0254d360f4b0f97dbb981a44caee7968daa7
Tena Spoljar <tena.spoljar@gmail.com>
2024-04-29 11:32:05 +0200

    feat: create todos route and page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit app, app made in SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/todos">Todos</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
+
